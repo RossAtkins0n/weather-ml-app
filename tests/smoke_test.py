c21aa212ab49f4c4b8e328526dc0c9ca8ab70334
Ubuntu <ubuntu@ip-172-31-24-222.ec2.internal>
2025-11-21 17:02:06 +0000

    Update tests in tests folder

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,20 +1,24 @@
 import unittest
 from app import app
 
-class TestAppSmoke(unittest.TestCase):
-	def setUp(self):
-		app.testing = True
-		self.client = app.test_client()
-	
-	# Complete the function below to test a success in running the application
-	def test_prediction_route_success(self):
-		response = self.client.get('/')
-		
 
-	# Complete the function below to test a form is rendered
-	def test_get_form(self):
-		response = self.client.get('/')
-		
- 
+class TestAppSmoke(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.client = app.test_client()
+
+    # Test that the app runs and returns HTTP 200 on the home page
+    def test_prediction_route_success(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    # Test that the form is actually rendered on the page
+    def test_get_form(self):
+        response = self.client.get('/')
+        # check that there is a <form> tag in the returned HTML
+        self.assertIn(b'<form', response.data)
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
+
